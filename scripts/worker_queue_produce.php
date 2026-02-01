09fa1f5f3b43a53b5f3ba5dd86a276ed112f0b67
James Claw (deploy) <deploy@coos.eu>
2026-02-01 02:12:27 +0100

    Fix worker_queue_produce to use functions_v3

--- scripts/worker_queue_produce.php
+++ scripts/worker_queue_produce.php
@@ -2,7 +2,7 @@
 // Produce a worker_queue job (open) if none is currently open/claimed.
 // Intended to run via Linux cron.
 
-require_once __DIR__ . '/../public/functions.inc.php';
+require_once __DIR__ . '/../public/functions_v3.inc.php';
 
 $pdo = db();
 
